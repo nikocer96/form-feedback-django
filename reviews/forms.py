5c712c81cfcc9b8b4e2a0342de67be0ad6362c9b
nikocer96 <nikocerone96@gmail.com>
2025-02-05 14:31:04 +0100

    Added another in form

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class ReviewForm(forms.Form):
     username_form = forms.CharField(label="Your Name", max_length=10)
+    review_text = forms.CharField(label="Your Text", widget=forms.Textarea)
+    rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
